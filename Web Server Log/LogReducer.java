fe66db5599f2fd5ffb032ce07590d1a68c24a155
codιng вloĸe <500060811@stu.upes.ac.in>
2019-12-09 11:41:59 +0530

    Add files via upload

---
+++ Web Server Log/LogReducer.java
@@ -0,0 +1,24 @@
+package WebServerLog;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+public class LogReducer extends Reducer<Text, IntWritable, Text, IntWritable> 
+{
+	 
+	@Override	
+	 public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			
+     int total = 0;
+		for (IntWritable value : values) {
+			total += value.get();
+		}
+		context.write(key, new IntWritable(total));
+	}	
+}
